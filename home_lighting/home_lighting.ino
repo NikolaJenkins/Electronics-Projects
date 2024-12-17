7a7e9eed6ed9f85b3d49944a39abd065322c72b6
naojoy <njoy3663@student.pps.net>
2024-12-16 21:17:36 -0800

    home lighting

--- home_lighting/home_lighting.ino
+++ home_lighting/home_lighting.ino
@@ -1,123 +1,65 @@
+/// @file    ColorPalette.ino
+/// @brief   Demonstrates how to use @ref ColorPalettes
+/// @example ColorPalette.ino
+
 #include <FastLED.h>
-/* https://randomnerdtutorials.com/guide-for-ws2812b-addressable-rgb-led-strip-with-arduino/
-led strip tutorial */
 
-#define ledPin 4
-#define ledCount 14 //change to match actual number of leds
-#define brightness 64
-#define ledType WS2811
-#define colorOrder GRB
-CRGB leds[ledCount];
-#define updatesPerSecond 100
+#define LED_PIN     4
+#define NUM_LEDS    14
+#define LIGHT_THRESHOLD 300
+CRGB leds[NUM_LEDS];
 
-CRGBPalette16 currentPalette;
-TBlendType    currentBlending;
-
-extern CRGBPalette16 myRedWhiteBluePalette;
-extern const TProgmemPalette16 myRedWhiteBluePalette_p PROGMEM;
-
-#define pirPin 5
-#define pirState = 0
+const int pirPin = 5;
+const int lightPin = A0;
+int pirState = 0;
 
 void setup() {
-  // put your setup code here, to run once:
   pinMode(pirPin, INPUT);
+  pinMode(lightPin, INPUT);
   Serial.begin(9600);
-
   delay( 3000 ); // power-up safety delay
-  FastLED.addLeds<LED_TYPE, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection( TypicalLEDStrip );
-  FastLED.setBrightness(  BRIGHTNESS );
-    
-  currentPalette = RainbowColors_p;
-  currentBlending = LINEARBLEND;
+  FastLED.addLeds<WS2812B, LED_PIN, GRB>(leds, NUM_LEDS);
+  FastLED.setBrightness(50);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  int lightState = analogRead(A0);
+
+void loop()
+{
+  int lightState = analogRead(lightPin);
   Serial.print("Light value: ");
   Serial.println(lightState);
-  if (lightState < 450) {
-    //turn led strip on
+  pirState = digitalRead(pirPin);  
+  if (lightState < LIGHT_THRESHOLD) {
+    if (pirState == HIGH) {
+      seePerson();
+    } else {
+      dimLevel(lightState);
+    }
   } else {
-    //turn led strip off
+    fill_solid(leds, NUM_LEDS, CRGB::Black);
   }
-  pirState = digitalRead(pirPin);
-  if (pirState == HIGH) {
-    Serial.println("motion detected");
-    //turn led strip on
-  } else {
-    Serial.println("no motion detected");
-    //turn led strip off
-  }
+}
 
-  ChangePalettePeriodically();
-    
-    static uint8_t startIndex = 0;
-    startIndex = startIndex + 1; /* motion speed */
-    
-    FillLEDsFromPaletteColors( startIndex);
-    
+void dimLevel(int brightness) {
+  int level = map(brightness, 0, LIGHT_THRESHOLD, 0, 255);
+  for (int i = 0; i < NUM_LEDS; i++) {
+    leds[i] = CRGB(level/3, level*2/3, level);
     FastLED.show();
-    FastLED.delay(1000 / UPDATES_PER_SECOND);
+    delay(50);
+    leds[i] = CRGB::Black;
+  }
+  for (int i = NUM_LEDS - 1; i >= 0; i--) {
+    leds[i] = CRGB(level/3, level*2/3, level);
+    FastLED.show();
+    delay(50);
+    leds[i] = CRGB::Black;
+  }
 }
 
-void FillLEDsFromPaletteColors( uint8_t colorIndex)
-{
-    uint8_t brightness = 255;
-    
-    for( int i = 0; i < NUM_LEDS; i++) {
-        leds[i] = ColorFromPalette( currentPalette, colorIndex, brightness, currentBlending);
-        colorIndex += 3;
-    }
+void seePerson() {
+  fill_solid(leds, NUM_LEDS, CRGB::Red);
+  FastLED.show();
+  delay(5000);
+  fill_solid(leds, NUM_LEDS, CRGB::Black);
+  FastLED.show();
 }
-
-void ChangePalettePeriodically()
-{
-    uint8_t secondHand = (millis() / 1000) % 60;
-    static uint8_t lastSecond = 99;
-    
-    if( lastSecond != secondHand) {
-        lastSecond = secondHand;
-        if( secondHand ==  0)  { currentPalette = RainbowColors_p;         currentBlending = LINEARBLEND; }
-        if( secondHand == 10)  { currentPalette = RainbowStripeColors_p;   currentBlending = NOBLEND;  }
-        if( secondHand == 15)  { currentPalette = RainbowStripeColors_p;   currentBlending = LINEARBLEND; }
-        if( secondHand == 20)  { SetupPurpleAndGreenPalette();             currentBlending = LINEARBLEND; }
-        if( secondHand == 25)  { SetupTotallyRandomPalette();              currentBlending = LINEARBLEND; }
-        if( secondHand == 30)  { SetupBlackAndWhiteStripedPalette();       currentBlending = NOBLEND; }
-        if( secondHand == 35)  { SetupBlackAndWhiteStripedPalette();       currentBlending = LINEARBLEND; }
-        if( secondHand == 40)  { currentPalette = CloudColors_p;           currentBlending = LINEARBLEND; }
-        if( secondHand == 45)  { currentPalette = PartyColors_p;           currentBlending = LINEARBLEND; }
-        if( secondHand == 50)  { currentPalette = myRedWhiteBluePalette_p; currentBlending = NOBLEND;  }
-        if( secondHand == 55)  { currentPalette = myRedWhiteBluePalette_p; currentBlending = LINEARBLEND; }
-    }
-}
-
-void SetupTotallyRandomPalette()
-{
-    for( int i = 0; i < 16; i++) {
-        currentPalette[i] = CHSV( random8(), 255, random8());
-    }
-}
-
-const TProgmemPalette16 myRedWhiteBluePalette_p PROGMEM =
-{
-    CRGB::Red,
-    CRGB::Gray, // 'white' is too bright compared to red and blue
-    CRGB::Blue,
-    CRGB::Black,
-    
-    CRGB::Red,
-    CRGB::Gray,
-    CRGB::Blue,
-    CRGB::Black,
-    
-    CRGB::Red,
-    CRGB::Red,
-    CRGB::Gray,
-    CRGB::Gray,
-    CRGB::Blue,
-    CRGB::Blue,
-    CRGB::Black,
-    CRGB::Black
-};
